7b5007318afe3bba3b32eeaf47af74e72e72a2c3
JoaksCodes <125510235+JoaksCodes@users.noreply.github.com>
2023-06-21 15:30:04 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let num1 = 8;
+let num2 = 3;
+let resultado;
+
+resultado = num1 + num2;
+console.log("Suma:", resultado);
+
+resultado = num1 - num2;
+console.log("Resta:", resultado);
+
+resultado = num1 * num2;
+console.log("Producto:", resultado);
+
+resultado = num1 / num2;
+console.log("División:", resultado);
+
+resultado = num1 % 2 === 0 ? "es par" : "es impar";
+console.log("El primer número", resultado);
+
+resultado = num2 % 2 === 0 ? "es par" : "es impar";
+console.log("El segundo número", resultado);
